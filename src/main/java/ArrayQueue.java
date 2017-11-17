16617d43ef26f037065fae03c653f92bd967c0ff
aparc <a.parchukov@yanex.ru>
2017-11-17 13:06:17 +0300

    added comments

--- src/main/java/ArrayQueue.java
+++ src/main/java/ArrayQueue.java
@@ -18,15 +18,17 @@ public class ArrayQueue<T> implements CustomQueue<T> {
     public void enqueue(T value) {
 
         if(!isFull()) {
-            rear = (rear == array.length - 1) ? -1 : rear; // cyclic tranfer
+            rear = (rear == array.length - 1) ? -1 : rear; // cyclic transfer
             array[++rear] = value;
             nElem++;
         } else {
             Object[] oldArray = array;
             array = new Object[(int)(array.length * 1.5)];
-            if(rear < front) {
+            if(rear < front) { // if the rear is lower than the front, expand the array:
                 System.arraycopy(oldArray, front, array, 0, oldArray.length - front);
+                // first add items from the front to latter symbol of array,
                 System.arraycopy(oldArray, 0, array, oldArray.length - front, rear + 1);
+                //  then add items from first symbol of to the rear
                 front = 0;
                 rear = oldArray.length - 1;
                 array[++rear] = value;
@@ -61,7 +63,7 @@ public class ArrayQueue<T> implements CustomQueue<T> {
         return (nElem == array.length);
     }
 
-    public boolean isEmpty() {
+    public boolean isEmpty() { // check queue empty
         return (nElem == 0);
     }
 }
