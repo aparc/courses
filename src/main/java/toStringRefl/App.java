75b4c2333480268bfa3d7408edc2b7baefbcbc93
aparc <a.parchukov@yandex.ru>
2017-11-25 23:38:11 +0300

    initial commit

---
+++ src/main/java/toStringRefl/App.java
@@ -0,0 +1,9 @@
+package toStringRefl;
+
+public class App {
+
+    public static void main(String[] args) throws IllegalAccessException {
+
+        StringUtils.toString(new SomeClass());
+    }
+}
---
+++ src/main/java/toStringRefl/SomeClass.java
@@ -0,0 +1,14 @@
+package toStringRefl;
+
+public class SomeClass {
+
+    private int number;
+    private String line;
+    private Object obj;
+
+    {
+        number = 1;
+        line = "quqq";
+        obj = new Object();
+    }
+}
