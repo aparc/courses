62081514e76927591189dd22348ba8698e2e3a1c
aparc <a.parchukov@yanex.ru>
2017-11-28 17:30:56 +0300

    init

--- src/main/java/toStringRefl/App.java
+++
@@ -1,9 +0,0 @@
-package toStringRefl;
-
-public class App {
-
-    public static void main(String[] args) throws IllegalAccessException {
-
-        System.out.println(StringUtils.toString(new SomeClass()));
-    }
-}
